ce191582846f902d2a4ef3a677e0caa1ff9e8840
Rishav Choudhary <rishavkcy@gmail.com>
2026-09-02 12:06:00 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        seen = {}
+
+        for i, n in enumerate(nums):
+            if n in seen:
+                seen[n] += 1
+            else:
+                seen[n] = 1
+        return max(seen, key=seen.get)                
+        
